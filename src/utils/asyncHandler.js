732aa74529aa18a97537155557f2404e223cef1a
krish-wp <202301189@daiict.ac.in>
2026-02-04 14:50:55 +0530

    feat: enhance asyncHandler to improve error handling and logging

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,13 @@
 const asyncHandler = (func) => {
   return (req, res, next) => {
     Promise.resolve(func(req, res, next)).catch((error) => {
-      next(error);
+      if (typeof next === 'function') {
+        return next(error);
+      }
+      console.error(error);
+      if (res && typeof res.status === 'function') {
+        return res.status(500).json({ message: 'Internal Server Error' });
+      }
     });
   };
 };
