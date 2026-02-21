cc7a7840b76978b37e9ea389cc1c77fc4789a03a
krish-wp <202301189@daiict.ac.in>
2026-02-21 21:48:17 +0530

    feat: integrate multer middleware for file uploads in comment and dump routes

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -8,14 +8,15 @@ import {
 } from '../contollers/comment.controllers.js';
 import { requireCommentOwner } from '../midddlewares/commentOwnership.middleware.js';
 import { requireSession } from '../midddlewares/requireSession.middleware.js';
+import { upload } from '../midddlewares/multer.middleware.js';
 
 const router = Router({ mergeParams: true });
 
-router.route('/').get(listComments).post(createComment);
+router.route('/').get(listComments).post(upload.none(), createComment);
 router
   .route('/:commentId')
   .get(getCommentById)
-  .patch(requireCommentOwner, updateComment)
+  .patch(upload.none(), requireCommentOwner, updateComment)
   .delete(requireCommentOwner, deleteComment);
 
 export default router;
--- src/routes/dump.routes.js
+++ src/routes/dump.routes.js
@@ -10,12 +10,13 @@ import commentRouter from './comment.routes.js';
 
 import { requireSession } from '../midddlewares/requireSession.middleware.js';
 import { requireDumpOwner } from '../midddlewares/dumpOwnership.middleware.js';
+import { upload } from '../midddlewares/multer.middleware.js';
 
 const router = Router();
 
 router.use(requireSession);
 
-router.route('/').get(listDumps).post(createDump);
+router.route('/').get(listDumps).post(upload.none(), createDump);
 router
   .route('/:dumpId')
   .get(getDumpById)
