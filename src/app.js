bc5d9bf8cf4cc15e034fccfcde3b273402262a9d
krish-wp <202301189@daiict.ac.in>
2026-02-24 21:08:06 +0530

    feat: update CORS configuration to include frontend URL

--- src/app.js
+++ src/app.js
@@ -13,7 +13,11 @@ app.use(express.static('public'));
 
 app.use(
   cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: [
+      'http://127.0.0.1:5500',
+      'http://localhost:5500',
+      'https://dumpit-frontend.onrender.com/',
+    ],
     credentials: true,
   })
 );
